eca73215a3ddd0eb7c9626274df9555dfe6438fe
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-05-01 13:22:12 -0700

    Make picker work in android and web

--- src/elements/DropdownInput/DropdownInput.tsx
+++ src/elements/DropdownInput/DropdownInput.tsx
@@ -10,12 +10,13 @@ import {
 	ViewStyle,
 	TouchableWithoutFeedback,
 	Modal,
+	Platform,
 } from 'react-native';
 import {
 	InputLabel,
 	Icon,
 } from '@elements';
-import { LIGHT_YELLOW } from '@util/colors';
+import { LIGHT_YELLOW, WHITE_TRANSPARENT } from '@util/colors';
 import styles from './DropdownInput.styles';
 
 interface DropdownInputProps {
@@ -50,52 +51,9 @@ export const DropdownInput = ({
 	return (
 		<View style={style}>
 			<InputLabel text={label} />
-
-			<TouchableWithoutFeedback onPress={handlePress}>
-				<View style={[
-					styles.dropdownInput,
-					{
-						flexDirection: 'row',
-						justifyContent: 'space-between',
-					},
-				]}
-				>
-					<Text style={styles.textColor}>{value}</Text>
-
-					<Icon name="dropdown" size={18} />
-				</View>
-			</TouchableWithoutFeedback>
-
-
-			<Modal
-				visible={isPickerOpen}
-				transparent={true}
-				onRequestClose={() => setIsPickerOpen(false)}
-			>
-				<View
-					style={{
-						flex: 1,
-					}}
-				>
-					<TouchableWithoutFeedback
-						onPress={() => { setIsPickerOpen(false); }}
-					>
-						<View
-							style={{
-								flex: 1,
-							}}
-						/>
-					</TouchableWithoutFeedback>
-
-					<View
-						style={{
-							// flex: 1,
-							width: '100%',
-							backgroundColor: LIGHT_YELLOW,
-							position: 'absolute',
-							bottom: 0,
-						}}
-					>
+			{
+				Platform.OS === 'android' || Platform.OS === 'web'
+					? (
 						<Picker
 							style={[
 								styles.picker,
@@ -109,9 +67,80 @@ export const DropdownInput = ({
 						>
 							{data && data.map(st => <Picker.Item color="NAVY_BLUE" label={st} value={st} />)}
 						</Picker>
-					</View>
-				</View>
-			</Modal>
+					)
+					: (
+						<View>
+							<TouchableWithoutFeedback onPress={handlePress}>
+								<View style={[
+									styles.dropdownInput,
+									{
+										flexDirection: 'row',
+										justifyContent: 'space-between',
+									},
+								]}
+								>
+									<Text style={styles.textColor}>{value}</Text>
+
+									<Icon name="dropdown" size={18} />
+								</View>
+							</TouchableWithoutFeedback>
+
+							<Modal
+								visible={isPickerOpen}
+								transparent={true}
+								onRequestClose={() => setIsPickerOpen(false)}
+							>
+								<View
+									style={{
+										flex: 1,
+										backgroundColor: WHITE_TRANSPARENT,
+									}}
+								>
+									<TouchableWithoutFeedback
+										onPress={() => { setIsPickerOpen(false); }}
+									>
+										<View
+											style={{
+												flex: 1,
+											}}
+										/>
+									</TouchableWithoutFeedback>
+
+									<View
+										style={{
+											// flex: 1,
+											width: '100%',
+											backgroundColor: LIGHT_YELLOW,
+											position: 'absolute',
+											bottom: 0,
+										}}
+									>
+										<Picker
+											style={[
+												styles.picker,
+												{
+													// height: 300,
+												},
+											]}
+											itemStyle={styles.pickerItemStyle}
+											selectedValue={value}
+											onValueChange={handleOnValueChange}
+										>
+											{data && data.map(st => (
+												<Picker.Item
+													color="NAVY_BLUE"
+													label={st}
+													value={st}
+													key={st}
+												/>
+											))}
+										</Picker>
+									</View>
+								</View>
+							</Modal>
+						</View>
+					)
+			}
 		</View>
 	);
 };
